96a52a25663a4ba00f116f7715c490b28074252b
lela2011 <54556055+lela2011@users.noreply.github.com>
2024-09-18 09:12:39 +0200

    Only allow _blank as target

--- resources/views/components/head/tinymce-config.blade.php
+++ resources/views/components/head/tinymce-config.blade.php
@@ -7,6 +7,8 @@
         elementpath: false, // removes the hint which html tag is currently used
         plugins: 'link lists',
         toolbar: 'undo redo | blocks | bold italic underline | link | bullist numlist | alignleft aligncenter alignright | indent outdent',
-        link_assume_external_targets: true
+        link_assume_external_targets: true,
+        default_link_target: "_blank",
+        target_list: [ {title: 'New page', value: '_blank', selected: true} ],
     });
 </script>
